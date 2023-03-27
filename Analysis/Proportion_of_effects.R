a24ceb18063e86cf416d2534bd41f4cddc084faf
oksanabuzh <oksana.buzh@fu-berlin.de>
2023-03-27 07:08:26 +0200

    add proportion of effects

---
+++ Analysis/Proportion_of_effects.R
@@ -0,0 +1,89 @@
+# Proportions of effects
+library(tidyverse)
+
+# 1. for the analysis in the main text----
+
+df_main <- read_csv("Results/mod_main_text.csv")
+str(df_main)
+
+
+group <- read_csv ("Data/EF_grouped.csv")
+names(group)
+
+dat_perc <- merge(df_main, group)
+dat_perc
+str(dat_perc)
+
+# Create new columns needed for the further analysis
+dat_perc <- dat_perc %>% 
+  mutate(Signif=case_when(p < 0.050 ~ "Signif", p >= 0.050 ~ "Non-sign")) %>% # create column with significant vs non-significant effects
+  mutate(Eff_sighn=case_when(effect_size_st < 0 ~ "negative", p >= 0 ~ "positive")) # create column with direction of effect (positive vs negative)
+
+dat_perc
+
+# summarise data
+
+count <- dat_perc %>% 
+  count(predictor, Dimens, AG_BG, Signif, Eff_sighn) %>% 
+  add_count(predictor, Dimens, wt=sum(n), name = "sum") %>% 
+  filter(Signif=="Signif")
+count
+
+summarised <- count %>% 
+  pivot_wider(names_from = "AG_BG", values_from = "n") %>%
+  mutate(AG = ifelse(is.na(AG), 0, AG),     
+         AG_BG = ifelse(is.na(AG_BG), 0, AG_BG),
+         BG = ifelse(is.na(BG ), 0, BG )) %>% 
+  mutate(Total=AG+AG_BG+BG) %>% 
+  mutate(AG_pr=round(AG*100/sum, digits = 2)) %>% 
+  mutate(BG_pr=round(BG*100/sum, digits = 2)) %>% 
+  mutate(Total_pr=round(Total*100/sum, digits = 2)) %>% 
+  select(-Signif )
+
+summarised
+
+write_csv (summarised, "Results/Summary_Effects_MainText.csv")
+
+
+# 2. for the analysis in the supplementary ----
+
+df_SI <- read_csv("Results/mod_supp.csv")
+str(df_SI)
+
+
+group <- read_csv ("Data/EF_grouped.csv")
+names(group)
+
+dat_perc_SI <- merge(df_SI, group)
+dat_perc_SI
+str(dat_perc_SI)
+
+# Create new columns needed for the further analysis
+dat_perc_SI <- dat_perc_SI %>% 
+  mutate(Signif=case_when(p < 0.050 ~ "Signif", p >= 0.050 ~ "Non-sign")) %>% # create column with significant vs non-significant effects
+  mutate(Eff_sighn=case_when(effect_size_st < 0 ~ "negative", p >= 0 ~ "positive")) # create column with direction of effect (positive vs negative)
+
+dat_perc_SI
+
+# summarise data
+
+count_SI <- dat_perc_SI %>% 
+  count(predictor, Dimens, AG_BG, Signif, Eff_sighn) %>% 
+  add_count(predictor, Dimens, wt=sum(n), name = "sum") %>% 
+  filter(Signif=="Signif")
+count_SI
+
+summarised_SI <- count_SI %>% 
+  pivot_wider(names_from = "AG_BG", values_from = "n") %>%
+  mutate(AG = ifelse(is.na(AG), 0, AG),     
+         AG_BG = ifelse(is.na(AG_BG), 0, AG_BG),
+         BG = ifelse(is.na(BG ), 0, BG )) %>% 
+  mutate(Total=AG+AG_BG+BG) %>% 
+  mutate(AG_pr=round(AG*100/sum, digits = 2)) %>% 
+  mutate(BG_pr=round(BG*100/sum, digits = 2)) %>% 
+  mutate(Total_pr=round(Total*100/sum, digits = 2)) %>% 
+  select(-Signif )
+
+summarised_SI
+
+write_csv (summarised_SI, "Results/Summary_Effects_SI.csv")
--- Analysis/check_my_results.R
+++ Analysis/check_my_results.R
@@ -11,6 +11,7 @@ Index <- Index %>%
 
 # Example 1 -----
 
+
 # Model for the main text:----
 # the target predictor is fitted first (but always after block)
 # for this we use anova()  -- type I
@@ -134,7 +135,7 @@ df_SI %>%
 
 
 
-# all results match completely:
+# all results match completely
 
 
 ##############-
@@ -247,3 +248,141 @@ df_SI %>%
 
 # the results of the comparisons match the Example 1, except that the Estimate now is the slope for block 1, while in the Example 1 it is the intercept
 # but what we need is that the Estimate should be the slope of our turget predictor
+
+
+
+################
+
+
+# AG.Herbivores_AG.Carnivores??????????????????????ß
+
+# Example 3 -----
+
+
+# Model for the main text:----
+# the target predictor is fitted first (but always after block)
+# for this we use anova()  -- type I
+
+m1<-lm(AG.Herbivores_AG.Carnivores ~  block +  log2(sowndiv) + leg.ef + gr.ef + sh.ef + th.ef, data=Index)
+
+anova(m1)
+summary(m1)
+
+
+par(mfrow = c(2, 2))
+plot(m1)
+par(mfrow = c(1, 1))
+
+bc <- MASS::boxcox(m1, data = Index, plotit = FALSE)
+lambda <- bc$x[which.max(bc$y)]
+lambda
+# [1] 0.4
+
+#fit new linear regression model using the Box-Cox transformation
+library(tidyverse)
+Index <- Index %>% 
+  mutate(AG.Herbivores_AG.Carnivores_c = (AG.Herbivores_AG.Carnivores ^ lambda - 1) / lambda)
+
+Index$AG.Herbivores_AG.Carnivores_c
+
+m2<-lm(AG.Herbivores_AG.Carnivores_c ~  block +  log2(sowndiv) + leg.ef + gr.ef + sh.ef + th.ef, data=Index)
+anova(m2)
+summary(m2)
+
+# Back transform of the effect size:----
+# 1. predict from m1 for the max level of the target predictor variable (i.e., sowndiv) -> y_max
+# 2. predict from m1 for the min level of the target predictor variable (i.e., sowndiv) -> y_min
+# 3. back-transform the predicted values -> back_y_max  and  back_y_min
+# 4. calculate the back-transformed size of the effect:
+# back_tr_predicted_estimate = (back_y_max - back_y_min)/ (x_max - x_min)
+
+
+# 1.
+newdat1 <- expand.grid(sowndiv = max(Index$sowndiv), 
+                       block = unique(factor(Index$block)), 
+                       leg.ef=mean(Index$leg.ef),
+                       gr.ef=mean(Index$gr.ef),
+                       sh.ef=mean(Index$sh.ef),
+                       th.ef=mean(Index$th.ef))
+pred1 <- predict(m2, newdata = newdat1)
+y_max <- mean(pred1)
+y_max
+
+# 2.
+newdat2 <- expand.grid(sowndiv = min(Index$sowndiv), 
+                       block = unique(factor(Index$block)), 
+                       leg.ef=mean(Index$leg.ef),
+                       gr.ef=mean(Index$gr.ef),
+                       sh.ef=mean(Index$sh.ef),
+                       th.ef=mean(Index$th.ef))
+pred2 <- predict(m2, newdata = newdat2)
+y_min <- mean(pred2)
+y_min
+
+# 3.
+back_y_min <- (y_min * lambda + 1)^(1 / lambda)
+back_y_max <- (y_max * lambda + 1)^(1 / lambda)
+
+#4.
+x_max <- max(Index$sowndiv)
+x_min<- min(Index$sowndiv)
+back_tr_predicted_estimate = (back_y_max - back_y_min)/ (x_max - x_min)
+back_tr_predicted_estimate
+
+
+# Standardize the back transformed effect size:----
+# Standardization is implemented by multiplying the effect size by the ratio of 
+# the standard deviation of x over the standard deviation in y.
+x <- Index$sowndiv
+y <- Index$AG.Herbivores_AG.Carnivores
+st_estimate <- back_tr_predicted_estimate * (sd(x)/sd(y))
+st_estimate
+
+
+# Compare with the results derived by the function: ----
+
+df_main <- read.csv("Results/mod_main_text.csv")
+
+
+lambda
+coef(m2)["log2(sowndiv)"] # estimate
+anova(m2)["log2(sowndiv)", "Pr(>F)"] # p value
+#R2 model
+summary(m2)$r.squared 
+#R2 partial
+r2glmm::r2beta(m2, method = "nsj", partial = TRUE)%>%
+  filter(Effect == "sowndiv_log2") %>% pull(Rsq) %>% round(3)
+
+back_tr_predicted_estimate #  back-transformed effect size
+
+st_estimate # standard. effect size (back-transformed)
+
+
+
+df_main %>% 
+  filter(response=="AG.Herbivores_AG.Carnivores", predictor=="sowndiv") %>% 
+  select(lambda, estimate, p, R2_model, r2_part, effect_size, effect_size_st)
+
+
+# check the range of the standardized coefficients
+df_main %>%
+  summarise(min=min(effect_size_st), max=max(effect_size_st), mean=mean(effect_size_st))
+
+# compare with the supplementary ----
+# for this we use car::Anova()  -- type II
+# the difference in the SI from the  analysis in the main text sould be  only in p value:
+
+car::Anova(m2)["log2(sowndiv)", "Pr(>F)"] # p value
+
+# result derived by the function
+df_SI <- read.csv("Results/mod_supp.csv")
+df_SI %>% 
+  filter(response=="AG.Herbivores_AG.Carnivores", predictor=="sowndiv") %>% 
+  select(p)
+
+m3<-lm(AG.Herbivores_AG.Carnivores_c ~  block + leg.ef + gr.ef + sh.ef + th.ef +  log2(sowndiv) , data=Index)
+anova(m3)
+car::Anova(m3)
+summary(m3)
+
+# all results match completely
